503403281e0f404d31254d9cc71749e3b3811d0b
zahern <zeke.ahern@hdr.qut.edu.au>
2024-10-08 13:43:39 +1000

    reformatting

--- MetaCount/test_code_for_package/predicted_r.R
+++
@@ -1,8 +0,0 @@
-# Load the data
-data <- read.csv("1848.csv")
-
-# Fit a Poisson GLM to the data
-model <- glm(FSI ~ US + S, data = data, family = poisson)
-
-# View the model summary
-summary(model)
