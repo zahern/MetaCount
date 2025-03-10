24c085dedc3603b3e4e0b26fc1f6b2c45e834a15
zahern <zeke.ahern@hdr.qut.edu.au>
2025-03-10 15:52:29 +1000

    X type issue fin

--- metacountregressor/solution.py
+++ metacountregressor/solution.py
@@ -1446,7 +1446,7 @@ class ObjectiveFunction(object):
         #disp = sm.OLS(ab.ravel(), bb.ravel()).fit()
         #gamma = disp.params[0]
         #print(f'dispersion is {gamma}')
-        gamma = np.min(dispersion,1)
+        gamma = np.min((dispersion,1))
         if gamma < 0.05:
             gamma = 0.05
         return gamma
