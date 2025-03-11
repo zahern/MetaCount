8b30bc6854c73838cfeeff2dfaf56eefa453e410
zahern <zeke.ahern@hdr.qut.edu.au>
2025-03-11 17:54:32 +1000

    new whee

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setuptools.setup(
     license='MIT',
     packages=['metacountregressor'],
     zip_safe=False,
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=[
         'numpy>=1.13.1',
         'scipy>=1.0.0',
