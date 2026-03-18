cb0106f84a33aebb9532ab9b2db10ad1306e194f
zahern <z.ahern@qut.edu.au>
2026-03-18 11:00:27 +1000

    aa

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("version.txt") as f:
+    version = f.read().strip()
+
 setup(
     name="metacountregressor",
     version=open("version.txt").read().strip(),
